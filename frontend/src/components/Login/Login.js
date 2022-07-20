7f99416ec123e02197526028ed4e34889b066dbe
kshitij <kmeshram@kapitaldata.com>
2022-07-20 17:47:01 +0530

    Changed Login and Register file: We can get the Login and Register data from the frontend

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Login.css";
 
 const Login = () => {
+    
+    const [user, setUser ] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleChange = e => {
+        const {name, value} = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+    
     return (
         <div className='Login'>
         <h1>Login</h1>
-        <input type="text" placeholder='Enter your Email'></input>
-        <input type="Password" placeholder='Enter your Password'></input>
+        <input type="text" name="email" value={user.email} onChange={handleChange} placeholder='Enter your Email'></input>
+        <input type="Password" name="password" value={user.password} onChange={handleChange} placeholder='Enter your Password'></input>
         <div className='Button'>Login</div>
         <div>or</div>
         <div className='Button'>Register</div>
--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Register.css";
 
 const Register = () => {
+
+    const [user, setUser ] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        cpassword: ""
+    })
+
+    const handleChange = e => {
+        const {name, value} = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
     return (
         <div className='Register'>
+        {console.log(user)}
         <h1>Register</h1>
-        <input type="text" placeholder='Enter your Firstname'></input>
-        <input type="text" placeholder='Enter your Lastname'></input>
-        <input type="text" placeholder='Enter your Email'></input>
-        <input type="Password" placeholder='Enter your Password'></input>
-        <input type="Password" placeholder='Re-Enter your Password'></input>
+        <input type="text" name="firstname" value={user.firstname} placeholder='Enter your Firstname' onChange={ handleChange }></input>
+        <input type="text" name="lastname" value={user.lastname} placeholder='Enter your Lastname' onChange={ handleChange }></input>
+        <input type="text" name="email" value={user.email} placeholder='Enter your Email' onChange={ handleChange }></input>
+        <input type="Password" name="password" value={user.password} placeholder='Enter your Password' onChange={ handleChange }></input>
+        <input type="Password" name="cpassword" value={user.cpassword} placeholder='Re-Enter your Password' onChange={ handleChange }></input>
         <div className='Button'>Register</div>
         <div>or</div>
         <div className='Button'>Login</div>
